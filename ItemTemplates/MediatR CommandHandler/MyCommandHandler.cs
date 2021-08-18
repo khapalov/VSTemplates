4ed72abff45955696f678d4061e094bd31dd87a9
Хапалов Сергей <khapalov@outlook.com>
2021-08-18 11:23:20 +0300

    MediatR command handler

---
+++ ItemTemplates/MediatR CommandHandler/MyCommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace $rootnamespace$
+{
+    public class $safeitemname$ : IRequestHandler<$fileinputname$Command, $fileinputname$Response>
+    {
+        public Task<$fileinputname$Response> Handle($fileinputname$Command request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ItemTemplates/MediatR CommandHandler/MyResponse.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace $rootnamespace$
+{
+    public class $safeitemname$
+    {
+    }
+}
